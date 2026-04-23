880d894d4eacaf936df01430864417a04ac453b7
bhj665@myy.haaga-helia.fi <bhj665>
2026-04-23 20:55:47 +0300

    Spring Boot projekti luotu

---
+++ src/main/java/thesis/musiikkipalvelu/MusiikkipalveluApplication.java
@@ -0,0 +1,13 @@
+package thesis.musiikkipalvelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiikkipalveluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiikkipalveluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thesis/musiikkipalvelu/MusiikkipalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package thesis.musiikkipalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiikkipalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
